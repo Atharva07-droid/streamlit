ae308b368b51a070a3b1b6db25dd3211aa85d3c4
Atharva Kapade <69674477+Atharva07-droid@users.noreply.github.com>
2021-01-10 12:00:54 +0530

    Update AK.py

--- AK.py
+++ AK.py
@@ -3,16 +3,19 @@ import streamlit as st
 
 st.write("""
 ## Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Tesla!
+Shown are the stock **closing price** and ***volume*** of your stock!
 """)
+
+user_input = st.text_input("Enter name of stock", "TSLA")
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'TSLA'
+# tickerSymbol = 'TSLA'
+tickerSymbol = user_input
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-01-10', end='2021-01-10')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write("""
